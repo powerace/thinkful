036f5be4016c3f1635c0aefe91a0fd07dd83d2a0
Powerace <nieshar@gmail.com>
2014-11-08 21:53:07 -0600

    refactor fizzbuzz

--- fizzbuzz/main.js
+++ fizzbuzz/main.js
@@ -23,24 +23,57 @@ $(document).ready(function(){
 
 	numArray.shift(); */
 
+	var fizzBuzzer = function(endNum){
 
-	for(var i=0; i <= 100; i++){
-		numArray.push(i + " ");
-		if ((numArray[i]%15)==0) {
-			numArray[i] = "fizzbuzz ";
+			for(var i=0; i <= endNum; i++){
+				numArray.push(i + " ");
+				if ((numArray[i]%15)==0) {
+					numArray[i] = "fizzbuzz ";
 
-		} else if (numArray[i]%3==0) {
-			numArray[i] = "fizz ";
-		} else if (numArray[i]%5==0) {
-			numArray[i] = "buzz ";
+				} else if (numArray[i]%3==0) {
+					numArray[i] = "fizz ";
+				} else if (numArray[i]%5==0) {
+					numArray[i] = "buzz ";
+				}
+			} 
+	}; 
+
+
+	$('input').keydown(function(event){
+
+		if(event.keyCode == 13 && userInput !== ''){
+
+			var userInput = $(this).val();
+			var endNum = +userInput;
+
+			if (isNaN(endNum)){
+				alert("That's not a number.");
+				
+
+			} else {
+				fizzBuzzer(endNum);
+			} 
+
+			
+			
+
+
+			
+
+
+			numArray.shift(); 
+			$('body').append(numArray);  
+
 		}
 
-	}
-	
-	numArray.shift(); 
+		
+
+
+	});
 
 	
-	$('body').append(numArray);
+
+	
 
 
 });
